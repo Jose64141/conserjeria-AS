94d3a7c0f1a6959ae48ad60910315d29ab751252
Jose Alcayaga <jose64am@aol.com>
2023-11-26 02:27:31 -0300

    feat: added basic grpc service

--- src/main/java/cl/ucn/disc/as/Main.java
+++ src/main/java/cl/ucn/disc/as/Main.java
@@ -1,19 +1,14 @@
 package cl.ucn.disc.as;
 
-import cl.ucn.disc.as.model.Departamento;
-import cl.ucn.disc.as.model.Edificio;
-import cl.ucn.disc.as.model.Persona;
-import cl.ucn.disc.as.services.Sistema;
-import cl.ucn.disc.as.services.SistemaImpl;
+import cl.ucn.disc.as.grpc.PersonaGrpcServiceImpl;
 import cl.ucn.disc.as.ui.ApiRestServer;
-import cl.ucn.disc.as.ui.RoutesConfigurator;
 import cl.ucn.disc.as.ui.WebController;
-import io.ebean.DB;
-import io.ebean.Database;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.javalin.Javalin;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.Instant;
+import java.io.IOException;
 
 /**
  *
@@ -24,15 +19,24 @@ public final class Main {
      * Entrypoint
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException  {
 
         log.debug("Starting Main ..");
 
+        log.debug("Starting the REST API server ..");
         Javalin app = ApiRestServer.start(7070, new WebController());
 
         //log.debug("Stopping ..");
         //app.stop();
 
+        log.debug("Starting the gRPC server ..");
+        Server server = ServerBuilder
+                .forPort(50123)
+                .addService(new PersonaGrpcServiceImpl())
+                .build();
+        server.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+        server.awaitTermination();
         log.debug("Done.");
     }
 }
--- src/main/java/cl/ucn/disc/as/ui/WebController.java
+++ src/main/java/cl/ucn/disc/as/ui/WebController.java
@@ -1,9 +1,15 @@
 package cl.ucn.disc.as.ui;
 
+import cl.ucn.disc.as.grpc.PersonaGrpc;
+import cl.ucn.disc.as.grpc.PersonaGrpcRequest;
+import cl.ucn.disc.as.grpc.PersonaGrpcResponse;
+import cl.ucn.disc.as.grpc.PersonaGrpcServiceGrpc;
 import cl.ucn.disc.as.model.Persona;
 import cl.ucn.disc.as.services.Sistema;
 import cl.ucn.disc.as.services.SistemaImpl;
 import io.ebean.DB;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
 
@@ -43,5 +49,32 @@ public final class WebController implements RoutesConfigurator {
             ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find Persona with rut: " + rut)));
         });
 
+        app.get("/grpc/personas/rut/{rut}", ctx -> {
+            String rut = ctx.pathParam("rut");
+
+            ManagedChannel channel = ManagedChannelBuilder
+                    .forAddress("localhost", 50123)
+                    .usePlaintext()
+                    .build();
+
+            PersonaGrpcServiceGrpc.PersonaGrpcServiceBlockingStub stub =
+                    PersonaGrpcServiceGrpc.newBlockingStub(channel);
+
+            PersonaGrpcResponse response = stub.retrieve(PersonaGrpcRequest
+                    .newBuilder()
+                    .setRut(rut)
+                    .build());
+
+            PersonaGrpc personaGrpc = response.getPersona();
+
+            Optional<Persona> oPersona = Optional.of(Persona.builder()
+                    .rut(personaGrpc.getRut())
+                    .nombre(personaGrpc.getNombre())
+                    .apellidos(personaGrpc.getApellidos())
+                    .email(personaGrpc.getEmail())
+                    .telefono(personaGrpc.getTelefono())
+                    .build());
+            ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find Persona with rut: " + rut)));
+        });
     }
 }
